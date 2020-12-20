19315461c098713a6b0a13dad31bd3ee052e24f3
lDiego257 <lDiego257>
2020-12-20 17:21:46 -0400

    Escenario 313 green

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ pub fn kata4(arr: &mut [i32]) -> Vec<i32>{
         }
         arr.swap(smallest,left);
     }
-    let mut temp = arr.to_vec();
+    let mut temp = Vec::new();
+    for i in 0..len-1{
+        if arr[i] != arr[i+1]{
+            temp.push(arr[i]);
+        }
+    }
+    temp.push(arr[len-1]); 
     temp
 }
 
@@ -31,4 +37,10 @@ mod tests{
        let actual = super::kata4(&mut array);
        assert_eq!(actual, [1,2,3,4,5]);
   }
+  #[test]
+  fn escenario313(){
+      let mut array: [i32;6] = [3,3,2,2,1,1];
+      let actual = super::kata4(&mut array);
+      assert_eq!(actual, [1,2,3]);
+ }
 }

c657b36887731514e1f6b05d872e154854924fa3
lDiego257 <lDiego257>
2020-12-20 17:13:57 -0400

    Escenario 311 green

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn kata4(arr: &mut [i32]) -> Vec<i32>{
+    let mut temp = vec![1,2,3];
+    temp
+}
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn escenario311(){
+        let mut array: [i32;3] = [3,2,1];
+        let actual = super::kata4(&mut array);
+        assert_eq!(actual, [1,2,3]);
+   }
+}
